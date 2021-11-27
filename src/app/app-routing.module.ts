93c498663ae62765be42d2a82d179c1ccbc7620a
OskarHabeych <habeysanoi@gmail.com>
2021-11-27 13:14:55 -0500

    commit 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './assets/error/error.component';
+import { IndexComponent } from './assets/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/index'
+  },
+  {
+    path: 'seguridad',
+    loadChildren: () => import('./modulos/seguridad/seguridad.module').then(m => m.SeguridadModule)
+  },{
+    path: 'admin',
+    loadChildren: () => import('./modulos/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'cliente',
+    loadChildren: () => import('./modulos/cliente/cliente.module').then(m => m.ClienteModule)
+  },
+  {
+    path: 'encomienda',
+    loadChildren: () => import('./modulos/encomienda/encomienda.module').then(m => m.EncomiendaModule)
+  },
+  {
+    path: 'servicios',
+    loadChildren: () => import('./modulos/servicios/servicios.module').then(m => m.ServiciosModule)
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+  },{
+    path: '**',
+    redirectTo: '/error'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
